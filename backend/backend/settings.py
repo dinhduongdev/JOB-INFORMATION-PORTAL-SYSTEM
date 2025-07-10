2d01a6d34b9910daa2a3a66ae1209f9f53a3d9e0
dinhduongdev <duongxummo@gmail.com>
2025-07-11 00:41:59 +0700

    UI login + register

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2o!1)@47=pl74@*gw+lh5g^*@e9oh1*-(-7@l*!@f#zx2g*twd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
