bdab0d695d8741042fd8478788f1b2ba2a8c85e5
Nguyễn Quang Huy <huykhongcan04@gmail.com>
2025-07-23 01:25:43 +0700

    rename "Apis" to "apis"

--- frontend/src/features/applicant-profile/applicantProfileActions.js
+++ frontend/src/features/applicant-profile/applicantProfileActions.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { endpoints, authApis } from "../../configs/Apis";
+import { endpoints, authApis } from "../../configs/apis";
 
 export const fetchApplicantProfile = createAsyncThunk(
   'applicantProfile/fetchApplicantProfile',
