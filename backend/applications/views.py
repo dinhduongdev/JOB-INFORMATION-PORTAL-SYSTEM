31a81cb9702275da2f92043807611d9820c9b0a3
Nguyễn Quang Huy <huykhongcan04@gmail.com>
2025-08-20 18:48:02 +0700

    delete file on s3

--- backend/applications/views.py
+++ backend/applications/views.py
@@ -1,14 +1,20 @@
-from rest_framework import mixins, viewsets, status
+from rest_framework import mixins, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from jobs.models import JobPost
+from user.permissions import IsAdminOrOwnerEmployer, IsApplicant
+
 from .models import CV, Application
-from .permissions import HasApplicantProfile, IsApplicant, OwnerOfApplicantProfilePath
-from .serializers import CVSerializer, ApplicationReadSerializer, ApplicationWithApplicantSerializer
-from user.permissions import IsApplicant, IsAdminOrOwnerEmployer
+from .permissions import HasApplicantProfile, OwnerOfApplicantProfilePath
+from .serializers import (
+    ApplicationReadSerializer,
+    ApplicationWithApplicantSerializer,
+    CVSerializer,
+)
+from proxies.s3.utils import delete_object
 
 
 class CVViewSet(
@@ -26,42 +32,64 @@ class CVViewSet(
 
     def get_permissions(self):
         PERMISSION_MAP = {
-            "create": [IsAuthenticated, IsApplicant, HasApplicantProfile, OwnerOfApplicantProfilePath],
-            "list": [IsAuthenticated, IsApplicant, HasApplicantProfile, OwnerOfApplicantProfilePath],
+            "create": [
+                IsAuthenticated,
+                IsApplicant,
+                HasApplicantProfile,
+                OwnerOfApplicantProfilePath,
+            ],
+            "list": [
+                IsAuthenticated,
+                IsApplicant,
+                HasApplicantProfile,
+                OwnerOfApplicantProfilePath,
+            ],
             "retrieve": [IsAuthenticated],
-            "destroy": [IsAuthenticated, IsApplicant, HasApplicantProfile, OwnerOfApplicantProfilePath],
+            "destroy": [
+                IsAuthenticated,
+                IsApplicant,
+                HasApplicantProfile,
+                OwnerOfApplicantProfilePath,
+            ],
         }
         permissions = PERMISSION_MAP.get(self.action)
         return [permission() for permission in permissions]
+
+    def perform_destroy(self, instance):
+        
+        delete_object(instance.link)
+        return super().perform_destroy(instance)
 
 
 class ApplicationViewSet(viewsets.ModelViewSet):
     queryset = Application.objects.all()
 
     def get_serializer(self, *args, **kwargs):
-        if self.action in ['by_job']:
+        if self.action in ["by_job"]:
             return ApplicationWithApplicantSerializer(*args, **kwargs)
-        elif self.action in ['my_applications']:
+        elif self.action in ["my_applications"]:
             return ApplicationReadSerializer(*args, **kwargs)
         return super().get_serializer(*args, **kwargs)
 
     def get_permissions(self):
-        if self.action in ['my_applications']:
+        if self.action in ["my_applications"]:
             return [IsApplicant()]
-        elif self.action in ['by_job']:
+        elif self.action in ["by_job"]:
             return [IsAdminOrOwnerEmployer()]
         else:
             return [IsAuthenticated]
 
-    @action(detail=False, methods=["get"], url_path='my-applications')
+    @action(detail=False, methods=["get"], url_path="my-applications")
     def my_applications(self, request):
         user = request.user
 
-        applications = self.queryset.filter(applicant=user.applicant_profile).select_related("job_post")
+        applications = self.queryset.filter(
+            applicant=user.applicant_profile
+        ).select_related("job_post")
         serializer = self.get_serializer(applications, many=True)
         return Response(serializer.data)
 
-    @action(detail=False, methods=['get'], url_path='by_job/(?P<job_id>[^/.]+)')
+    @action(detail=False, methods=["get"], url_path="by_job/(?P<job_id>[^/.]+)")
     def by_job(self, request, job_id=None):
         """
         GET /api/applications/by_job/{job_id}/
