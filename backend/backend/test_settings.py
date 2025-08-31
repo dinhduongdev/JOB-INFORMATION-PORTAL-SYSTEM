c9e4843ba00babfd631af2cd83cac6b93f3e1316
Nguyễn Quang Huy <huykhongcan04@gmail.com>
2025-08-31 22:36:28 +0700

    settings for testing

--- backend/backend/test_settings.py
+++ backend/backend/test_settings.py
@@ -10,10 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
-import json
 from os import getenv
 from pathlib import Path
-import sys
 
 from django.conf.global_settings import CSRF_TRUSTED_ORIGINS
 
@@ -25,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = getenv("SECRET_KEY")
+SECRET_KEY = "afowehjfouiaywhldmnvoaerhfowjef;laksdjf;.as,d"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = getenv("DEBUG", "True") == "True"
@@ -109,20 +107,10 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": getenv("DB_ENGINE"),
-        "NAME": getenv("DB_NAME"),
-        "USER": getenv("DB_USER"),
-        "PASSWORD": getenv("DB_PASSWORD"),
-        "HOST": getenv("DB_HOST"),
-        "PORT": getenv("DB_PORT"),
-        "OPTIONS": {
-            "sslmode": getenv("DB_SSLMODE", "disable"),
-            "channel_binding": getenv("DB_CHANNEL_BINDING", "disable"),
-        },
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
     },
 }
-if "test" in sys.argv:
-    DATABASES["default"] = {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"}
 
 AUTH_USER_MODEL = "user.User"
 ACCOUNT_ACTIVATION_TIMEOUT_SEC = int(getenv("ACCOUNT_ACTIVATION_TIMEOUT_SEC", 86400))
